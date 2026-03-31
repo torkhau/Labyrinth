8628aeca84d98163f9e29461c6b5a3af50af2263
Aliaksandr Torkhau <torkhau@gmail.com>
2026-03-31 17:02:54 +0200

    style: update footer styles and formatting

--- src/components/Layouts/Footer/Footer.tsx
+++ src/components/Layouts/Footer/Footer.tsx
@@ -1,15 +1,15 @@
-import './Footer.css'
+import './Footer.css';
 
 export function Footer() {
   return (
-      <footer>
-        <h2>Connect with me</h2>
-        <a href='https://github.com/torkhau' target='_blank'>
-          <svg className='button-icon' role='presentation' aria-hidden='true'>
-            <use href='/icons.svg#github-icon'></use>
-          </svg>
-          GitHub
-        </a>
-      </footer>
-  )
+    <footer>
+      <h2>Connect with me</h2>
+      <a href='https://github.com/torkhau' target='_blank'>
+        <svg className='button-icon' role='presentation' aria-hidden='true'>
+          <use href='/icons.svg#github-icon'></use>
+        </svg>
+        GitHub
+      </a>
+    </footer>
+  );
 }
